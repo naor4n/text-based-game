839fc3bd6f9c5443afd1e3163c66a98c1c6431d2
naor4n <147795804+naor4n@users.noreply.github.com>
2023-12-10 17:33:09 +0000

    adding final changes

--- text-game.py
+++ text-game.py
@@ -1,3 +1,4 @@
+import random
 
 class bcolors:
     HEADER = '\033[95m'
@@ -10,51 +11,59 @@ class bcolors:
 
 class Player:
     def __init__(self):
-        self.__inventory = []
+        self.inventory = []
 
     def check_inventory(self):
-        print(self.__inventory)
+        print(self.inventory)
     
     def add_to_inventory(self, item):
-        self.__inventory.append(item)
+        self.inventory.append(item)
 
     def is_item(self, check):
-        if check in self.__inventory:
+        if check in self.inventory:
             return True
         else:
             return False
         
     def condition(self):
-        self.__condition = False
+        self.condition = False
 
 class Objects:
-    def __init__(self, place, item):
+    def __init__(self, place, items):
         self.__place = place
-        self.__item = item
-        self.__place_inventory = []
-        self.__place_inventory.append(item)
+        self.place_inventory = items
 
+    def take_item(self, item):
+        self.item = item
+        player.add_to_inventory(item)
+        self.place_inventory.remove(self.item)    
 
-    def take_item(self):
-        player.add_to_inventory(self.__item)
-        self.__place_inventory.remove(self.__item)
-        pass
-
-    def place_item(self):
-        Player.__inventory.remove(self.__item)
-        pass
+    def take_item_rnd(self):
+        item = random.choice(self.place_inventory)
+        player.add_to_inventory(item)
+        self.place_inventory.remove(item)
 
     def check_items(self):
-        return len(self.__place_inventory)
+        return len(self.place_inventory)
 
 
 player = Player()
-alley = Objects("Alley", "fish")
+alley = Objects("Alley", ["fish"])
+kitten = Objects("Kitten", ["seashell", "banana", "spoon"])
+cat_kingdom = Objects("cat_kingdom", [])
 
 
 def final_route():
-    print("You go through the middle door, avoiding the humans you still hear. ")
+    print("\nYou go through the middle door, avoiding the humans you still hear. ")
     print("Behind the door is a brown spotted cat. You follow it to a cat flap, and go through.")
+
+    
+
+
+    print(bcolors.WARNING + "You've complited your first quest!!" + bcolors.ENDC)
+    exit()
+
+
 
 
 def route1():
@@ -62,18 +71,17 @@ def route1():
     print("You see a dumpster next to a suspicious door. This is the origin of the fishy smell.")
     print("You could probably manage to get in and out of the dumpster without getting stuck. Will you peek in?")
     while True:
-        ac1 = input(bcolors.HEADER + "Type 'peek' or 'turn back'\n" + bcolors.ENDC)
-        if ac1 == "peek":
+        a_choice_1 = input(bcolors.HEADER + "Type 'peek' or 'turn back'\n" + bcolors.ENDC)
+        if a_choice_1 == "peek":
             while alley.check_items() != 0:
                 print("\nYou see a bunch of fish inside. Take one?")
-                ac2 = input(bcolors.HEADER +"Type y/n\n"+ bcolors.ENDC)
-                if ac2 == "y":
-                    alley.take_item()
-                    player.check_inventory()
+                a_choice_2 = input(bcolors.HEADER +"Type y/n\n"+ bcolors.ENDC)
+                if a_choice_2 == "y":
+                    alley.take_item("fish")
                     print("\nYou take the fish and go back to the beginning.")
                     quest1()
                     
-                elif ac2 == "n":
+                elif a_choice_2 == "n":
                     print("\nYou turn around and go back to the beginning.")
                     quest1()
                     
@@ -81,7 +89,7 @@ def route1():
                 print(bcolors.WARNING + "\nYou can't reach far enough to take another fish so you turn back." + bcolors.ENDC)
                 quest1()
                 
-        elif ac1 == "turn back":
+        elif a_choice_1 == "turn back":
             quest1()
             
         else: 
@@ -94,53 +102,61 @@ def route2():
     print("Will you go straight ahead or along the fence to the right?")
 
     while True:
-        bc1 = input(bcolors.HEADER + "Type 'right' or 'straight'\n" + bcolors.ENDC)
-        if bc1 == "right":
+        b_choice_1 = input(bcolors.HEADER + "Type 'right' or 'straight'\n" + bcolors.ENDC)
+        if b_choice_1 == "right":
             print("\nYou're now next to a house. Through an open window you hear people talking.")
             if not player.condition:
                 print("Will you jump inside, or turn around?")
+
                 while True:
-                    bc2 = input(bcolors.HEADER + "Type 'jump' or 'turn around'\n" + bcolors.ENDC)
-                    if bc2 == "jump":
+                    b_choice_2 = input(bcolors.HEADER + "Type 'jump' or 'turn around'\n" + bcolors.ENDC)
+                    if b_choice_2 == "jump":
                         print(bcolors.WARNING + "\nLooking in from the windowsill, there are three doors inside. You don't which one to choose. You turn back." + bcolors.ENDC)
                         route2()
                         
-
-                    elif bc2 == "turn around":
+                    elif b_choice_2 == "turn around":
                         route2()
-                        
+
                     else:
                         print(bcolors.FAIL + "Please choose one of the options." + bcolors.ENDC)
             else:
                 print("Will you jump inside, or turn around?")
+
                 while True:
-                    bc4 = input(bcolors.HEADER + "Type 'jump' or 'turn around'\n" + bcolors.ENDC)
-                    if bc4 == "jump":
+                    b_choice_4 = input(bcolors.HEADER + "Type 'jump' or 'turn around'\n" + bcolors.ENDC)
+                    if b_choice_4 == "jump":
                         print("\nWhich door was the right one?")
                         door = input(bcolors.HEADER + "type 'left', 'middle' or 'right' \n" + bcolors.ENDC)
                         if door == "middle":
                             final_route()
                         else:
-                            print("wrong door, you were caught by the humans")
+                            print("\nwrong door, you were caught by the humans")
                             exit()
-                    elif bc2 == "turn around":
+
+                    elif b_choice_2 == "turn around":
                         route2()
                         
                     else:
                         print(bcolors.FAIL + "Please choose one of the options." + bcolors.ENDC)
             
 
-        elif bc1 == "straight":
+        elif b_choice_1 == "straight":
             print("\nYou see a scared kitten. Maybe if you offer it food, it will tell you where to go?")
+
             while True:
-                bc3 = input(bcolors.HEADER + "Type 'offer food'\n" + bcolors.ENDC)
-                if bc3 == "offer food":
+                b_choice_3 = input(bcolors.HEADER + "Type 'offer food'\n" + bcolors.ENDC)
+                if b_choice_3 == "offer food":
                     if player.is_item("fish"):
                         print("\nyou offer the fish from earlier")
+
+                        player.inventory.remove("fish")
                         player.condition = True
-                        print(player.condition)
+
                         print("The kitten gives you instructions to a place called ", bcolors.OKCYAN +"The Cat Kingdom" + bcolors.ENDC)
                         print("Head right from the previous crossing, go through the window and choose the middle door.")
+                        print("You are also given an item to pass through the gates once you arrive.")
+                        kitten.take_item_rnd()
+
                         route2()
                         
                     else:
@@ -172,6 +188,7 @@ def quest1():
 def main():
     player.condition = False
     print(bcolors.HEADER + "welcome, traveler! Would you like to embark on a journey?" + bcolors.ENDC)
+    
     while True:
         embark = input("y/n\n")
         if embark == "y":
@@ -182,7 +199,7 @@ def main():
             print(bcolors.BOLD + "Until next time..." + bcolors.ENDC)
             break
         else:
-            print(bcolors.FAIL + "Come again?" + bcolors.ENDC)
+            print(bcolors.FAIL + "What was that?" + bcolors.ENDC)
         
 
 main()
